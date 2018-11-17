f6998293c4d3319fb6372ef3355eb41a2dee8b8f
dmalchenko <malchenko.dmitriy.by@gmail.com>
2018-11-17 18:49:25 +0300

    add cert path

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ token = '703798310:AAHwlQf2T95-coxhZMrCpNZrMTgtWSC3e_U'
 db = dict(user='postgres', password='nginxphp7', host='localhost', port=5432, database='wialon_message_bot')
 callback_url = 'https://46.36.221.101/wialon/bot/notify?hash=%s'
 telegram_webhook_url = 'https://46.36.221.101/wialon/bot/index'
-cert_file = ''
+cert_file = '/etc/nginx/ssl/nginx.crt'
